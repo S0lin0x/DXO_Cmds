b924fb96d14c336dfc6be0d287952baa235b71ce
Aaron Rayfield <rayfield.aaron@gmail.com>
2018-09-12 17:07:27 -0700

    a

--- Stop-Dxo.sh
+++ Stop-Dxo.sh
@@ -1,20 +1,30 @@
+echo -e "Stopping Dxo node 1";
 dextro-cli -datadir=/root/.dextro stop
 sleep 1
+echo -e "Stopping Dxo node 2";
 dextro-cli -datadir=/root/.dextro2 stop
 sleep 1
+echo -e "Stopping Dxo node 3";
 dextro-cli -datadir=/root/.dextro3 stop
 sleep 1
+echo -e "Stopping Dxo node 4";
 dextro-cli -datadir=/root/.dextro4 stop
 sleep 1
+echo -e "Stopping Dxo node 5";
 dextro-cli -datadir=/root/.dextro5 stop
 sleep 1
+echo -e "Stopping Dxo node 6";
 dextro-cli -datadir=/root/.dextro6 stop
 sleep 1
+echo -e "Stopping Dxo node 7";
 dextro-cli -datadir=/root/.dextro7 stop
 sleep 1
+echo -e "Stopping Dxo node 8";
 dextro-cli -datadir=/root/.dextro8 stop
 sleep 1
+echo -e "Stopping Dxo node 9";
 dextro-cli -datadir=/root/.dextro9 stop
 sleep 1
+echo -e "Stopping Dxo node 10";
 dextro-cli -datadir=/root/.dextro10 stop
 sleep 1
