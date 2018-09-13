5129488d1fdcb5e15bb0b2bd6c1303b40a6cf3c0
Aaron Rayfield <rayfield.aaron@gmail.com>
2018-09-12 17:12:18 -0700

    a

--- Start-Dxo-nodes.sh
+++ Start-Dxo-nodes.sh
@@ -1,20 +1,30 @@
+echo -e "Starting Dxo node 1";
 dextrod -datadir=/root/.dextro -daemon
 sleep 1
+echo -e "Starting Dxo node 2";
 dextrod -datadir=/root/.dextro2 -daemon
 sleep 1
+echo -e "Starting Dxo node 3";
 dextrod -datadir=/root/.dextro3 -daemon
 sleep 1
+echo -e "Starting Dxo node 4";
 dextrod -datadir=/root/.dextro4 -daemon
 sleep 1
+echo -e "Starting Dxo node 5";
 dextrod -datadir=/root/.dextro5 -daemon
 sleep 1
+echo -e "Starting Dxo node 6";
 dextrod -datadir=/root/.dextro6 -daemon
 sleep 1
+echo -e "Starting Dxo node 7";
 dextrod -datadir=/root/.dextro7 -daemon
 sleep 1
+echo -e "Starting Dxo node 8";
 dextrod -datadir=/root/.dextro8 -daemon
 sleep 1
+echo -e "Starting Dxo node 9";
 dextrod -datadir=/root/.dextro9 -daemon
 sleep 1
+echo -e "Starting Dxo node 10";
 dextrod -datadir=/root/.dextro10 -daemon
 sleep 1
