3bd564bac68960f555037c6bd181c1328ef3a7a7
Aaron Rayfield <rayfield.aaron@gmail.com>
2018-09-12 17:04:10 -0700

    a
    a

---
+++ Start-Dxo.sh
@@ -0,0 +1,20 @@
+dextrod -datadir=/root/.dextro -daemon
+sleep 1
+dextrod -datadir=/root/.dextro2 -daemon
+sleep 1
+dextrod -datadir=/root/.dextro3 -daemon
+sleep 1
+dextrod -datadir=/root/.dextro4 -daemon
+sleep 1
+dextrod -datadir=/root/.dextro5 -daemon
+sleep 1
+dextrod -datadir=/root/.dextro6 -daemon
+sleep 1
+dextrod -datadir=/root/.dextro7 -daemon
+sleep 1
+dextrod -datadir=/root/.dextro8 -daemon
+sleep 1
+dextrod -datadir=/root/.dextro9 -daemon
+sleep 1
+dextrod -datadir=/root/.dextro10 -daemon
+sleep 1
---
+++ Stop-Dxo.sh
@@ -0,0 +1,22 @@
+dextro-cli -datadir=/root/.dextro stop
+sleep 1
+dextro-cli -datadir=/root/.dextro1 stop
+sleep 1
+dextro-cli -datadir=/root/.dextro2 stop
+sleep 1
+dextro-cli -datadir=/root/.dextro3 stop
+sleep 1
+dextro-cli -datadir=/root/.dextro4 stop
+sleep 1
+dextro-cli -datadir=/root/.dextro5 stop
+sleep 1
+dextro-cli -datadir=/root/.dextro6 stop
+sleep 1
+dextro-cli -datadir=/root/.dextro7 stop
+sleep 1
+dextro-cli -datadir=/root/.dextro8 stop
+sleep 1
+dextro-cli -datadir=/root/.dextro9 stop
+sleep 1
+dextro-cli -datadir=/root/.dextro10 stop
+sleep 1
