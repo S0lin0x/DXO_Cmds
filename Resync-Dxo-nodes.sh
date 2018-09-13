81775992da85b8d8dcf6f0cc58dee3d9ac82b2d9
Aaron Rayfield <rayfield.aaron@gmail.com>
2018-09-12 17:18:54 -0700

    a

---
+++ Resync-Dxo-nodes.sh
@@ -0,0 +1,40 @@
+echo -e "Removing 'blocks, chainstate and peers' from Dxo node 1";
+rm -r .dextro/blocks
+rm -r .dextro/chainstate
+rm -r .dextro/peers.dat
+echo -e "Removing 'blocks, chainstate and peers' from Dxo node 2";
+rm -r .dextro2/blocks
+rm -r .dextro2/chainstate
+rm -r .dextro2/peers.dat
+echo -e "Removing 'blocks, chainstate and peers' from Dxo node 3";
+rm -r .dextro3/blocks
+rm -r .dextro3/chainstate
+rm -r .dextro3/peers.dat
+echo -e "Removing 'blocks, chainstate and peers' from Dxo node 4";
+rm -r .dextro4/blocks
+rm -r .dextro4/chainstate
+rm -r .dextro4/peers.dat
+echo -e "Removing 'blocks, chainstate and peers' from Dxo node 5";
+rm -r .dextro5/blocks
+rm -r .dextro5/chainstate
+rm -r .dextro5/peers.dat
+echo -e "Removing 'blocks, chainstate and peers' from Dxo node 6";
+rm -r .dextro6/blocks
+rm -r .dextro6/chainstate
+rm -r .dextro6/peers.dat
+echo -e "Removing 'blocks, chainstate and peers' from Dxo node 7";
+rm -r .dextro7/blocks
+rm -r .dextro7/chainstate
+rm -r .dextro7/peers.dat
+echo -e "Removing 'blocks, chainstate and peers' from Dxo node 8";
+rm -r .dextro8/blocks
+rm -r .dextro8/chainstate
+rm -r .dextro8/peers.dat
+echo -e "Removing 'blocks, chainstate and peers' from Dxo node 9";
+rm -r .dextro9/blocks
+rm -r .dextro9/chainstate
+rm -r .dextro9/peers.dat
+echo -e "Removing 'blocks, chainstate and peers' from Dxo node 10";
+rm -r .dextro10/blocks
+rm -r .dextro10/chainstate
+rm -r .dextro10/peers.dat
